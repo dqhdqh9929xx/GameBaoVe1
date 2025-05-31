9c141fbdfba280606f622ac031ad8d0a13d3c80a
admin <admin@gmail.com>
2025-05-31 06:24:36 -0700

    Day6.2

--- Assets/scripts/PanelPlayerManager.cs
+++ Assets/scripts/PanelPlayerManager.cs
@@ -7,39 +7,27 @@ public class PanelPlayerManager : MonoBehaviour
     public Transform NoteBookFull;
 
     public bool hasInstantiated = false;
-    public float SpawnLocal = 100f;
-
-
+    public float SpawnLocal = 0f;
 
 
     void Update()
     {
-
         if (animationController.NextNv == true)
         {
             hasInstantiated = false;
             SpawnLocal += 100f;
+            Debug.Log("Da +");
         }
 
-        if (hasInstantiated == false)
+        if (hasInstantiated == false && animationController.id != 1)
         {
             animationController.NextNv = false;
             hasInstantiated = true;
-            if (NoteBookFull == null)
-            {
-                Debug.LogWarning("NoteBookFull chưa được gán!");
-                return;
-            }
-            // Lấy vị trí local của PanelPlayerManager so với NoteBookFull
             Vector3 localPos = NoteBookFull.InverseTransformPoint(transform.position);
-            // Giảm y đi 100 đơn vị trong local space
             Vector3 spawnLocalPos = new Vector3(localPos.x, localPos.y - SpawnLocal, localPos.z);
-            // Instantiate prefab (UI) làm con của NoteBookFull, không làm thay đổi scale/rotation
             GameObject newPrefab = Instantiate(prefabToInstantiate);
             newPrefab.transform.SetParent(NoteBookFull, false);
-            // Đặt vị trí local
             newPrefab.GetComponent<RectTransform>().localPosition = spawnLocalPos;
-
             Debug.Log("Đã instantiate prefab tại vị trí: " + spawnLocalPos);
 
         }
